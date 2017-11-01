19e6e6bd6e0e41994f4c94b43f65563c2fcbf28f
Shrikant <562971@cognizant.com>
2017-11-01 13:29:52 +0530

    master

--- Server.js
+++ Server.js
@@ -1,1 +1,14 @@
-//Hello
+/*
+Author Rajendra Shrikant
+*/
+var express = require('express');
+var http = require('http');
+var app = express();
+app.use(express.static(__dirname + '/')); 
+app.get('/',function(req, res){
+	res.sendfile("index.html")
+});
+var server_app = http.createServer(app);
+server_app.listen(process.env.PORT || 5000, function () {
+  console.log('listening on http://localhost:5000'); //8080
+});
